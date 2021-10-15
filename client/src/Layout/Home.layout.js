edbaf7b8a3082e55a3d12b698fd6d7c7cebe7f08
Gauri002 <gauripatil22001@gmail.com>
2021-10-15 22:46:13 +0530

    Done

--- client/src/Layout/Home.layout.js
+++ client/src/Layout/Home.layout.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React,{useEffect} from "react";
+import {useDispatch} from "react-redux";
 
 import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
+import { getRestaurant } from "../Redux/Reducer/restaurant/restaurant.action";
 
 const HomeLayout = (props) => {
+ const dispatch = useDispatch();
+
+ useEffect(() => {
+   dispatch(getRestaurant());
+ }, []);
+
     return <>
      <div className="container mx-auto lg:px-20">
          <Navbar />
     {props.children}
     </div>
+    <FoodTab/>
     </>
   };
   
